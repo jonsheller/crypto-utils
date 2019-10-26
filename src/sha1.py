05b625c315e40329d0be80158b53ad7418acb2b6
Jon Sheller <jonsheller@gmail.com>
2019-10-26 10:54:54 -0700

    black

--- src/sha1.py
+++ src/sha1.py
@@ -2,24 +2,20 @@
 import io
 import struct
 
-INITIAL_H = (0x67452301,
-             0xEFCDAB89,
-             0x98BADCFE,
-             0x10325476,
-             0xC3D2E1F0)
+INITIAL_H = (0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0)
 
 
 def leftrotate(v, n):
-    return 0xFFffFFff & ((0xFFffFFff & (v << n)) | (0xFFffFFff & (v >> (32 - n))))
+    return 0xFFFFFFFF & ((0xFFFFFFFF & (v << n)) | (0xFFFFFFFF & (v >> (32 - n))))
 
 
 def padding(data_length):
     pad = [0x80] + [0x0] * ((56 - (data_length + 1) % 64) % 64)
-    return bytes(pad) + struct.pack('>Q', data_length * 8)
+    return bytes(pad) + struct.pack(">Q", data_length * 8)
 
 
 class SHA1:
-    def __init__(self, h = INITIAL_H, data_len = 0):
+    def __init__(self, h=INITIAL_H, data_len=0):
         self._h = h
         self._unprocessed = bytes()
         self._data_len = data_len
@@ -44,14 +40,14 @@ class SHA1:
             if len(chunk) < 64:
                 break
             h = self._add_chunk(h, chunk)
-        return struct.pack('>5I', *h)
+        return struct.pack(">5I", *h)
 
     def _padding(self):
         total_len = self._data_len + len(self._unprocessed)
         return padding(total_len)
 
     def _add_chunk(self, h, chunk):
-        w = list(struct.unpack('>16I', chunk))
+        w = list(struct.unpack(">16I", chunk))
         for i in range(16, 80):
             w.append(leftrotate(w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16], 1))
 
@@ -59,18 +55,18 @@ class SHA1:
         for i in range(80):
             if i <= 19:
                 f = (b & c) | ((~b) & d)
-                k = 0x5a827999
+                k = 0x5A827999
             elif i <= 39:
                 f = b ^ c ^ d
-                k = 0x6ed9eba1
+                k = 0x6ED9EBA1
             elif i <= 59:
                 f = (b & c) | (b & d) | (c & d)
-                k = 0x8f1bbcdc
+                k = 0x8F1BBCDC
             else:
                 f = b ^ c ^ d
-                k = 0xca62c1d6
+                k = 0xCA62C1D6
 
-            t = 0xffFFffFF & (leftrotate(a, 5) + f + e + k + w[i])
+            t = 0xFFFFFFFF & (leftrotate(a, 5) + f + e + k + w[i])
             a, b, c, d, e = t, a, leftrotate(b, 30), c, d
 
         a += h[0]
@@ -79,8 +75,14 @@ class SHA1:
         d += h[3]
         e += h[4]
 
-        return (a & 0xffFFffFF, b & 0xffFFffFF, c & 0xffFFffFF, d & 0xffFFffFF, e & 0xffFFffFF)
+        return (
+            a & 0xFFFFFFFF,
+            b & 0xFFFFFFFF,
+            c & 0xFFFFFFFF,
+            d & 0xFFFFFFFF,
+            e & 0xFFFFFFFF,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/sha1_signature.py
+++ src/sha1_signature.py
@@ -4,7 +4,8 @@ from sha1 import SHA1, padding
 
 def print_internal_state(header, s):
     import struct
-    print(header, s._data_len, struct.pack('>5I', *s._h).hex())
+
+    print(header, s._data_len, struct.pack(">5I", *s._h).hex())
 
 
 def sign(secret, data):
@@ -21,7 +22,7 @@ def verify_signature(secret, data, signature):
 
 
 def extend(data, signature, total_length, additional_data):
-    h = [int(signature[8*i:8*i+8], 16) for i in range(5)]
+    h = [int(signature[8 * i : 8 * i + 8], 16) for i in range(5)]
     s = SHA1(h, total_length + len(padding(total_length)))
     s.update(additional_data)
     return data + padding(total_length) + additional_data, s.finalize().hex()
@@ -29,26 +30,26 @@ def extend(data, signature, total_length, additional_data):
 
 def step_by_step(key, data, additional_data):
     s = SHA1()
-    print_internal_state('init', s)
+    print_internal_state("init", s)
     s.update(key)
-    print_internal_state('key', s)
+    print_internal_state("key", s)
     s.update(data)
-    print_internal_state('data', s)
+    print_internal_state("data", s)
     s.update(padding(len(key) + len(data)))
-    print_internal_state('padding', s)
+    print_internal_state("padding", s)
     s.update(additional_data)
-    print_internal_state('additional', s)
-    print('final', s.finalize().hex())
+    print_internal_state("additional", s)
+    print("final", s.finalize().hex())
 
 
 def main():
-    d = b'a=1&b=2'
-    key = b'my secret key'
+    d = b"a=1&b=2"
+    key = b"my secret key"
     sig = sign(key, d)
-    nd, ns = extend(d, sig, len(d) + len(key), b'&b=5')
+    nd, ns = extend(d, sig, len(d) + len(key), b"&b=5")
     print(nd, ns, verify_signature(key, nd, ns))
-    step_by_step(key, d, b'&b=5')
+    step_by_step(key, d, b"&b=5")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
